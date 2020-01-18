9b08636bb705b1d3118df76dc20fa603f7680e8c
Cody Selman <cody.selman@sharpspring.com>
2020-01-17 19:22:36 -0500

    PlayerController added, walking implemented

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 1000.0f;
+    private Rigidbody rb;
+    private Vector3 moveDirection;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Update()
+    {
+        float horizontalMovement = Input.GetAxisRaw("Horizontal");
+        float verticalMovement = Input.GetAxisRaw("Vertical");
+
+        moveDirection = (horizontalMovement * transform.right + verticalMovement * transform.forward).normalized;
+    }
+
+    private void Move()
+    {
+        rb.velocity = moveDirection * speed * Time.deltaTime;
+    }
+}
