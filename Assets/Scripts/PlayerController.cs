1342f0eb38c38b6d1571dc91d4e69b9187a96a7b
Cody Selman <cody.selman@sharpspring.com>
2020-01-17 21:03:12 -0500

    added raycast logic

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,10 @@ public class PlayerController : MonoBehaviour
     private Rigidbody rb;
     private Vector3 moveDirection;
 
+    private float petRange = 3.0f;
+
+    public new Camera camera;
+
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
@@ -22,12 +26,34 @@ public class PlayerController : MonoBehaviour
     {
         float horizontalMovement = Input.GetAxisRaw("Horizontal");
         float verticalMovement = Input.GetAxisRaw("Vertical");
-
         moveDirection = (horizontalMovement * transform.right + verticalMovement * transform.forward).normalized;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            PetHandler();
+        }
+        
     }
 
     private void Move()
     {
         rb.velocity = moveDirection * speed * Time.deltaTime;
+    }
+
+    private void PetHandler()
+    {
+        RaycastHit hit;
+        Vector3 cameraPos = camera.transform.position;
+        Vector3 cameraForward = camera.transform.forward;
+        Debug.DrawRay(cameraPos, cameraForward * petRange);
+        bool isRaycastHit = Physics.Raycast(cameraPos, cameraForward, out hit, petRange);
+        if (isRaycastHit)
+        {
+            if (hit.collider.tag == "Pettable")
+            {
+                // handle pet here
+                Debug.Log("pet");
+            }
+        }
     }
 }
