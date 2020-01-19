ce041ed6f5dc8d98c404f341618fd0507200c76a
Cody Selman <cody.selman@sharpspring.com>
2020-01-18 19:03:42 -0500

    Added pause menu

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-
     private float minimumX = -60.0f;
     private float maximumX = 60.0f;
     private float sensitivityX = 5.0f;
@@ -14,24 +13,16 @@ public class CameraController : MonoBehaviour
 
     public new Camera camera;
 
-    private void Start()
-    {
-        Cursor.lockState = CursorLockMode.Locked;
-    }
-
     void Update()
     {
-        rotationY += Input.GetAxis("Mouse X") * sensitivityY;
-        rotationX += Input.GetAxis("Mouse Y") * sensitivityX;
-        rotationX = Mathf.Clamp(rotationX, minimumX, maximumX);
-
-        transform.localEulerAngles = new Vector3(0, rotationY, 0);
-        camera.transform.localEulerAngles = new Vector3(-rotationX, 0, 0);
-
-        if (Input.GetKey(KeyCode.Escape))
+        if (!GameController.isGamePaused)
         {
-            Cursor.lockState = CursorLockMode.None;
-            Cursor.visible = true;
+            rotationY += Input.GetAxis("Mouse X") * sensitivityY;
+            rotationX += Input.GetAxis("Mouse Y") * sensitivityX;
+            rotationX = Mathf.Clamp(rotationX, minimumX, maximumX);
+
+            transform.localEulerAngles = new Vector3(0, rotationY, 0);
+            camera.transform.localEulerAngles = new Vector3(-rotationX, 0, 0);
         }
     }
 }
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    public Button resumeButton;
+    public Button restartButton;
+    public Button quitButton;
+
+    public static bool isGamePaused = false;
+
+    private void Start()
+    {
+        Resume();
+    }
+
+    void Update()
+    {
+        if (isGamePaused)
+        {
+            if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.Return))
+            {
+                Resume();
+            } else if (Input.GetKeyDown(KeyCode.R))
+            {
+                SceneManager.LoadScene("Title");
+            } else if (Input.GetKeyDown(KeyCode.Q))
+            {
+                Application.Quit();
+            }
+        } else
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                Pause();
+            }
+        }
+    }
+
+    private void Resume()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        pauseMenu.SetActive(false);
+        isGamePaused = false;
+        Time.timeScale = 1f;
+    }
+
+    private void Pause()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isGamePaused = true;
+    }
+}
